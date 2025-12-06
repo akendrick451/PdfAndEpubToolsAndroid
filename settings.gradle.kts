4d83a103581f6fea8a237533fffded689bdbff78
akendrick451 <akendrick451@gmail.com>
2025-12-06 17:26:09 +1100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,31 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        // Add this line for epublib
+        maven { url = uri("https://github.com/psiegman/mvn-repo/raw/master/releases") }
+    }
+}
+
+rootProject.name = "PDFSetFilenameAKTools"
+include(":app")
+
+
+
+
+
